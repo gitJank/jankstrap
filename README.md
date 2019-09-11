27e341636f54a81bdf4719f1d6335149bcad6efe
gitJank <justinjankiewicz@gmail.com>
2019-09-11 09:41:47 -0500

    preperation for initial publish

---
+++ README.md
@@ -0,0 +1,1 @@
+## Custom material-ui based react components.
--- src/index.js
+++ src/index.js
@@ -2,26 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
 
-const SearchBar = ({ searchBy, onFilterTextChange }) => {
-  const classes = {
-    inputProps: {
-      height: "14px",
-      fontSize: "14px",
-      padding: "12px"
-    },
-    emptyLabelProps: {
-      marginTop: "-8px"
-    },
-    labelProps: { marginTop: "0px" },
-    searchButton: {
-      "&:hover": {
-        backgroundColor: "transparent"
-      }
+const useStyles = makeStyles(() => ({
+  inputProps: {
+    height: "14px",
+    fontSize: "14px",
+    padding: "12px"
+  },
+  emptyLabelProps: {
+    marginTop: "-8px"
+  },
+  labelProps: { marginTop: "0px" },
+  searchButton: {
+    "&:hover": {
+      backgroundColor: "transparent"
     }
-  };
+  }
+}));
+
+const SearchBar = ({ searchBy, onFilterTextChange }) => {
+  const classes = useStyles();
 
   return (
     <TextField
